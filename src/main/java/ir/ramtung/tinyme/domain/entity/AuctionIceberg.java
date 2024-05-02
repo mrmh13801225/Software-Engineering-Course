92c589ccf579be07ad11946748498654ac00af07
mobinahz <mobina.haghizadeh@ut.ac.ir>
2024-05-03 00:01:41 +0330

    Two new classes

---
+++ src/main/java/ir/ramtung/tinyme/domain/entity/AuctionIceberg.java
@@ -0,0 +1,21 @@
+package ir.ramtung.tinyme.domain.entity;
+
+import ir.ramtung.tinyme.messaging.request.EnterOrderRq;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+
+public class AuctionIceberg extends Order {
+
+    //fields
+
+    //constructors
+
+
+}
---
+++ src/main/java/ir/ramtung/tinyme/domain/entity/AuctionOrder.java
@@ -0,0 +1,19 @@
+package ir.ramtung.tinyme.domain.entity;
+
+import ir.ramtung.tinyme.messaging.request.EnterOrderRq;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+
+public class AuctionOrder extends IcebergOrder {
+
+    //fields
+
+    //constructors
+}
